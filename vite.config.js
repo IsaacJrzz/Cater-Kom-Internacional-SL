8577ab4d2a07439d95776a12791435bcb4e9f62a
ALONSO JUAREZ <iajuarez@segulagrp.es>
2026-04-16 13:01:46 +0200

    Fix: clean up gitignore and remove tracked node_modules/dist

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Cater-Kom-Internacional-SL/', // Nombre exacto de tu repo
 })
